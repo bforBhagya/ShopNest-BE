3a30e590d38685524d0904a5cde288f44d210b20
bforbhagya <bhagya.ariyarathne@gmail.com>
2023-11-15 21:30:43 +0530

    first commit

---
+++ src/main/java/com/ecws/E/commerce/Full/Stack/Web/Site/ECommerceFullStackWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ecws.E.commerce.Full.Stack.Web.Site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceFullStackWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceFullStackWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecws/E/commerce/Full/Stack/Web/Site/ECommerceFullStackWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecws.E.commerce.Full.Stack.Web.Site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceFullStackWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
